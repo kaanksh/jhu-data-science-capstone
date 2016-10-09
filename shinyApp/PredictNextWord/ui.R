45dba32104b573e5ceb199b68b40db78240da373
SSO <samy.soualem@gmail.com>
2016-10-09 22:30:55 +0200

    Added slide deck. Project submission version

--- shinyApp/PredictNextWord/ui.R
+++ shinyApp/PredictNextWord/ui.R
@@ -4,7 +4,7 @@ library(shiny)
 shinyUI(fluidPage(theme = "bootstrap.min.css",
   
   # Application title
-  titlePanel("Next word prediction with R"),
+  titlePanel("Predicting next words with R"),
 
   sidebarLayout(position = "left",
     sidebarPanel(
@@ -13,22 +13,22 @@ shinyUI(fluidPage(theme = "bootstrap.min.css",
       p("It is developed in R and it is the final project for the Data Science Specialization on Coursera."),
       
       h3("How to use it ?"),
-      p("Just start typing. The predictions are updated everytime you use a space."),
+      p("Just start typing. The predictions are updated after each space."),
       
       h3("Additional information"),
       div(
-        a("Data Science Specialization on Coursera by the Johns Hopkins University",
-        href="https://www.coursera.org/specializations/jhu-data-science")
+        a("Project presentation",
+          href="http://rpubs.com/ssoualem/jhu_capstone_presentation")
       ),
       div(
-        a("**** TODO : link to RPUBS slide deck ????? *****",
-          href="TODO")
+        a("Data Science Specialization on Coursera by the Johns Hopkins University",
+        href="https://www.coursera.org/specializations/jhu-data-science")
       )
     ),
       
     mainPanel(
       textInput("userTxt", "Type here :", ""),
-      h4("Next word predictions"),
+      h4("Most likely next words :"),
       textOutput("wordPredicted1", container = div),
       textOutput("wordPredicted2", container = div),
       textOutput("wordPredicted3", container = div),
